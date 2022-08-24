c29e4d1207fce4d6006289eacc7d9456397e4924
Klemen Slavič <krof.drakula@gmail.com>
2022-08-24 15:34:27 +0200

    Implement choosable locations

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
+import { Cmpx } from "./math";
 import { drawMandelbrot } from "./render";
 
-const limit = 1000;
-const size = 2048;
+const limit = 10e3;
+const size = 4096;
+
+const locations = new Map<string, [Cmpx, Cmpx]>([
+  [
+    "default",
+    [
+      [-2, -1.5],
+      [1, 1.5],
+    ],
+  ],
+  [
+    "web",
+    [
+      [-0.748 - 0.0014, 0.1 - 0.0014],
+      [-0.748 + 0.0014, 0.1 + 0.0014],
+    ],
+  ],
+]);
 
 const canvas = document.createElement("canvas");
 canvas.width = canvas.height = size;
@@ -11,9 +29,11 @@ canvas.style.cssText =
 
 document.body.append(canvas);
 
+const [topLeft, bottomRight] = locations.get("default")!;
+
 performance.mark("start");
 
-drawMandelbrot(canvas, [-2, -1.5], [1, 1.5], 1000).then(() => {
+drawMandelbrot(canvas, topLeft, bottomRight, 1000).then(() => {
   performance.mark("end");
   performance.measure("draw", "start", "end");
   console.log(
